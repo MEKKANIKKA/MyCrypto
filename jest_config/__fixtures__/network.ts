f2d1755553efb077576bf288a6135c210672423e
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-07-18 20:00:12 +0200

    Remove unused IAsset

--- jest_config/__fixtures__/network.ts
+++ jest_config/__fixtures__/network.ts
@@ -1,4 +1,4 @@
-import { Network, NodeType, TUuid, TSymbol, BlockExplorer } from '@types';
+import { Network, NodeType, TUuid, TTicker, BlockExplorer } from '@types';
 
 export const fNetwork: Network = {
   id: 'Ropsten',
@@ -41,7 +41,7 @@ export const fNetwork: Network = {
     '39a543b0-ac4f-5b14-9467-86fd6538a6a2'
   ] as TUuid[],
   baseAsset: '77de68da-ecd8-53ba-bbb5-8edb1c8e14d7' as TUuid,
-  baseUnit: 'RopstenETH' as TSymbol,
+  baseUnit: 'RopstenETH' as TTicker,
   nodes: [
     {
       name: 'ropsten_infura',
--- src/components/AccountSummary.tsx
+++ src/components/AccountSummary.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { truncate } from '@utils';
-import { TSymbol, TUuid } from '@types';
+import { TTicker, TUuid } from '@types';
 import Currency from './Currency';
 import Account from './Account';
 
@@ -13,7 +13,7 @@ interface Props {
   address: string;
   uuid?: TUuid;
   balance?: string;
-  assetSymbol?: string;
+  assetTicker?: TTicker;
   label?: string;
   hideCurrency?: boolean;
   onClick?(): void;
@@ -45,7 +45,7 @@ const SAccountWrapper = styled.div<StyleProps>`
 function AccountSummary({
   address,
   balance,
-  assetSymbol,
+  assetTicker,
   uuid,
   label,
   onClick,
@@ -57,7 +57,7 @@ function AccountSummary({
       {balance && uuid && (
         <SCurrency
           amount={balance}
-          symbol={(assetSymbol as TSymbol) || ('ETH' as TSymbol)}
+          ticker={assetTicker || ('ETH' as TTicker)}
           uuid={uuid}
           decimals={4}
           icon={true}
